fbe3b201b33e54dc78908a862d414f745c949d1d
Moviifox <moviifox.web@gmail.com>
2026-01-29 11:57:43 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Fix for iOS PWA viewport height issue
-const setViewportHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-
-// Set on load
-setViewportHeight();
-
-// Update on resize
-window.addEventListener('resize', setViewportHeight);
-window.addEventListener('orientationchange', setViewportHeight);
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
